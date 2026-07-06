2f4efdd76b9af38cfd0e271a084ef5faa851ea74
integrations-everbold <integrations@everboldmarketing.com>
2026-07-06 14:03:36 +0100

    Update test_meta_api.py

--- test_meta_api.py
+++ test_meta_api.py
@@ -1,1 +1,19 @@
+import os
+import requests
 
+TOKEN = os.environ["META_ACCESS_TOKEN"]
+AD_ACCOUNT_ID = os.environ["THR_AD_ACCOUNT_ID"]
+
+url = f"https://graph.facebook.com/v25.0/{AD_ACCOUNT_ID}/insights"
+
+params = {
+    "access_token": TOKEN,
+    "date_preset": "last_30d",
+    "level": "campaign",
+    "fields": "campaign_name,spend,impressions,reach,clicks,ctr,cpc,cpm,actions,cost_per_action_type"
+}
+
+response = requests.get(url, params=params)
+
+print("Status Code:", response.status_code)
+print(response.text[:5000])
